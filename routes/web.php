cfb3a70cec42118ee75f7210bfe5c6e4b40d2c53
Bishal Deb <102280193+bishald0013@users.noreply.github.com>
2025-08-06 22:20:35 +0530

    route changes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::redirect('/', '/login');
+Route::redirect('/', '/home');
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 
+//admin dashboard
 Route::get('/home', function () {
     return view('admin.dashboard');
 });
